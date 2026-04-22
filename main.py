bf14600705bfb57de61d2dc56ec1d3b6c0ec752b
jucaferla-eng <jucaferla@preferendum.com>
2026-04-22 09:32:33 -0400

    Update main.py

--- main.py
+++ main.py
@@ -211,8 +211,10 @@ class SecurityHeadersMiddleware(BaseHTTPMiddleware):
         )
 
         # Remove server info header
-        response.headers.pop('server', None)
-        response.headers.pop('x-powered-by', None)
+        if 'server' in response.headers:
+            del response.headers['server']
+        if 'x-powered-by' in response.headers:
+            del response.headers['x-powered-by']
 
         return response
 
